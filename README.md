85b8fba4130b250bff857f13fa506f7338e7239f
Adrian Negreanu <adrian.negreanu@nxp.com>
2021-08-28 15:13:34 +0300

    add dwt-to-gmon
    generate a gmon.out out of pc-addresses.
    
    Signed-off-by: Adrian Negreanu <adrian.negreanu@nxp.com>

--- README.md
+++ README.md
@@ -2,11 +2,30 @@
 
 Helpers I use toghether with OpenOCD and ARMGCC to run and debug on the mimxrt1170-evk board.
 
+ARMGCC's *bin* directory is assumed to be in $PATH.
+
+
 ### swo_parse.py
 
 No-dependency ITM Packet parser.
 By default it opens ./swo-out.txt. You can specify the input file with the --input argument.
 
+
 ### Gdbc.py
 
 It loads and starts an ELF file using the pygdbmi python module.
+
+
+### dwt-to-gmon.py
+
+OpenOCD can dump the SWO incoming packets to a file; swo_parse.py can parse that file and
+extract the Program Counters sampled by the DWT unit.
+These samples are then translated to a gmon.out file, using dwt-to-gmon.py.
+
+Here's an example of this process:
+
+```shell
+./parse_swo.py | grep dwt-pc: | sed -e 's/dwt-pc://' > dwt-pc.txt
+./dwt-to-gmon.py --input dwt-pc.txt
+arm-none-eabi-gprof.exe $binary_path/$binary_file > report.txt
+```
---
+++ dwt-to-gmon.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Adrian Negreanu
+
+import struct
+import argparse
+from ctypes import *
+from enum import IntEnum
+
+
+# From https://gist.github.com/lmyyao/355709b35b717c9e47c6795de7b45ccd
+def convert_struct_to_bytes(st):
+    buffer = create_string_buffer(sizeof(st))
+    memmove(buffer, addressof(st), sizeof(st))
+    return buffer.raw
+
+
+class RecordTag(IntEnum):
+    TIME_HIST = 0,# Histogram
+    CG_ARC = 1,   # Call-Graph Arc (not-supported)
+    BB_COUNT = 2  # Basic-Block Exec count (not-supported)
+
+
+class FileHeader(Structure):
+    _pack_ = 1
+    _fields_ = [("cookie", c_char * 4),
+                ("version", c_int32),
+                ("spare", c_char * 12)
+               ]
+
+
+class HistogramHeader(Structure):
+    _pack_ = 1
+    _fields_ = [("tag",  c_uint8),
+                ("low_pc", c_uint32),
+                ("high_pc", c_uint32),
+                ("hist_size", c_uint32),
+                ("prof_rate", c_uint32),
+                ("dimen", c_char * 15),
+                ("dimen_abbrev", c_char)
+               ]
+
+
+def read_dwt_pc(fname):
+    hist = {}
+    with open(fname, 'rt') as f:
+        for line in f:
+            addr = int(line,16)
+            if addr not in hist:
+                hist[addr] = 1
+            else:
+                hist[addr] += 1
+    return hist
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Consumes DWT PC-addresses and generates a gmon.out.")
+    parser.add_argument('--input', required=False, help='Input file with PC-addresses, one per line.', default='dwt-pc.txt')
+    args = parser.parse_args()
+
+    hist = read_dwt_pc(args.input)
+    hist_size = len(hist)
+
+    low_pc = min(hist.items(), key = lambda x: x[0])[0]
+    high_pc = max(hist.items(), key = lambda x: x[0])[0]
+    address_space = high_pc - low_pc
+    num_buckets = address_space // 2
+
+    # See the DWT CTRL setup in Gdbc.py
+    # only when you're interested in the actual time spent.
+    prof_rate = 140000
+
+    file_hdr = FileHeader(b"gmon", 0x00000001)
+    # +1 to account for the Histogram header too.
+    hist_hdr = HistogramHeader(RecordTag.TIME_HIST, low_pc, high_pc, num_buckets+1, prof_rate, b"seconds", b"s")
+
+    print(f'low_pc:{low_pc:x}')
+    print(f'high_pc:{high_pc:x}')
+    print(f'hist_size:{len(hist)}')
+    print("writing gmon.out")
+    with open('gmon.out', 'wb') as f:
+        f.write(convert_struct_to_bytes(file_hdr))
+        f.write(convert_struct_to_bytes(hist_hdr))
+        # +2 to include high_pc too.
+        for addr in range(low_pc, high_pc+2, 2):
+            value = 0
+            if addr in hist:
+                value = hist[addr]
+                del hist[addr]
+            f.write((value & 0xFFFF).to_bytes(2, byteorder='little'))
+
+    # addresses that were not dumped (there shouldn't be any)
+    for key,value in hist.items():
+        print(f"{key:x} {value}")
